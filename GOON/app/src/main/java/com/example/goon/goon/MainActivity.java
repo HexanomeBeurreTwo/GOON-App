ef5aeb510e9fc5e8c407194180a1e57b3c6aa988
dmitrica <dana11mitrica@gmail.com>
2016-04-26 11:16:31 +0200

    permissions for location added to AndroidManifest

--- GOON/app/src/main/java/com/example/goon/goon/MainActivity.java
+++ GOON/app/src/main/java/com/example/goon/goon/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.goon.goon;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -121,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Provider disabled", Toast.LENGTH_LONG).show();
         if ("GPS".equalsIgnoreCase(provider)) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
