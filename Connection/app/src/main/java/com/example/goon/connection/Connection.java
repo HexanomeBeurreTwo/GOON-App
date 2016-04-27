b6bf6bec4c485be392b25731a0cbf6afe141498f
dmitrica <dana11mitrica@gmail.com>
2016-04-27 18:05:17 +0200

    send data...pas vraiment testé

--- Connection/app/src/main/java/com/example/goon/connection/Connection.java
+++ Connection/app/src/main/java/com/example/goon/connection/Connection.java
@@ -1,61 +1,88 @@
 package com.example.goon.connection;
-
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.media.TimedText;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Message;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-
-
-
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.io.BufferedInputStream;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.DataInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import android.telecom.*;
+
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
 
 public class Connection extends AppCompatActivity {
 
-    Button buttonConnection;
+    private Button buttonConnection;
     private TextView text;
+    private EditText username;
+    private EditText password;
+    private EditText email;
+    private String usernameS="";
+    private String passwordS="";
+    private String emailS="";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         text= (TextView)findViewById(R.id.textView);
         buttonConnection= (Button) findViewById(R.id.button);
-
+        username = (EditText) findViewById(R.id.userName);
+        password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.email);
+        usernameS=username.getText().toString();
+        passwordS=username.getText().toString();
+        emailS=username.getText().toString();
 
         buttonConnection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkInternetConnection();
-                new JSONTask().execute("http://goonapp.herokuapp.com/helloworld");
+                if (checkInternetConnection()) {
+                   // new JSONTask().execute("http://goonapp.herokuapp.com/helloWorld");
+                    // User user = new User(username.toString(), password.toString(), email.toString());
+                    // postData("http://goonapp.herokuapp.com/user",  user);
+                    if(validate()) {
+                        new HttpAsyncTask().execute("https://goonapp.herokuapp.com/user");
+                    }else{
+
+                        Toast.makeText(getBaseContext(), "Enter some data!", Toast.LENGTH_LONG).show();
+                    }
+                }
+
             }
         });
     }
+
 
 
     public class JSONTask extends AsyncTask<String,String, String> {
@@ -102,6 +129,97 @@ public class Connection extends AppCompatActivity {
         }
     }
 
+    public static String POST(final String url, User user) throws JSONException, IOException {
+        InputStream inputStream= null;
+        String result = "";
+        String username=user.getUsername();
+        String password = user.getPassword();
+        String email = user.getEmail();
+
+        try {
+            JSONObject requestObject = new JSONObject();
+            requestObject.put("username", username);
+            requestObject.put("email", email);
+            requestObject.put("password", password);
+            String json = requestObject.toString();
+
+            HttpClient httpClient= new DefaultHttpClient();
+            HttpPost httpPost= new HttpPost(url);
+
+            StringEntity se = new StringEntity(json);
+            httpPost.setEntity(se);
+
+            httpPost.setHeader("GET", "/user/json");
+           // httpPost.setHeader("Content-type", "application/json");
+
+            HttpResponse httpResponse= httpClient.execute(httpPost);
+
+            inputStream=httpResponse.getEntity().getContent();
+
+            if (inputStream!=null){
+                result=convertInputStreamToString(inputStream);
+            }else{
+                result="Did not wor!k";
+            }
+        }catch(Exception e){
+
+            Log.d("InputStream", e.getLocalizedMessage());
+
+        }
+       return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream)throws IOException{
+
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line=bufferedReader.readLine())!=null){
+            result+=line;
+        }
+        inputStream.close();
+        return result;
+
+    }
+
+    private boolean validate(){
+        if(username.getText().toString().trim().equals(""))
+            return false;
+        else if(password.getText().toString().trim().equals(""))
+            return false;
+        else if(email.getText().toString().trim().equals(""))
+            return false;
+        else
+            return true;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            User user = new User();
+            user.setUsername(usernameS);
+            user.setUsername(passwordS);
+            user.setEmail(emailS);
+
+
+            try {
+                return POST(urls[0],user);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+
   /*  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -140,7 +258,7 @@ public class Connection extends AppCompatActivity {
         }else if (
                 connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
                         connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
-           // Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
+           Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
             return false;
         }
         return false;
---
+++ Connection/app/src/main/java/com/example/goon/connection/User.java
@@ -0,0 +1,71 @@
+package com.example.goon.connection;
+
+/**
+ * Created by Daniela on 27/04/2016.
+ */
+public class User {
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public User(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (!username.equals(user.username)) return false;
+        if (!password.equals(user.password)) return false;
+        return email.equals(user.email);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+}
